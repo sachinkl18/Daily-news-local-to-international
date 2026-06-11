3dc90cfe590de7a8f80a25df2390f2311b6694a8
sachinkl18 <sachingowdakl18@gmail.com>
2026-06-11 21:24:47 +0530

    Implement news fetching and rendering functionality

--- app.js
+++ app.js
@@ -1,3 +1,127 @@
+let allNews=[];
+
+fetch("news.json")
+.then(res=>res.json())
+.then(data=>{
+
+allNews=data;
+
+renderNews(data);
+
+if(data.length>0){
+
+document.getElementById(
+"breakingNews"
+).innerText=data[0].title;
+
+}
+
+});
+
+function renderNews(news){
+
+let container=
+document.getElementById(
+"news-container"
+);
+
+container.innerHTML="";
+
+news.forEach(article=>{
+
+container.innerHTML+=`
+
+<div class="news-card">
+
+<img src="${article.image}" alt="">
+
+<div class="news-content">
+
+<h2>${article.title}</h2>
+
+<p>${article.description}</p>
+
+<p>
+<b>Category:</b>
+${article.category || "General"}
+</p>
+
+<a href="${article.link}"
+target="_blank">
+Read More
+</a>
+
+</div>
+
+</div>
+
+`;
+
+});
+
+}
+
+document.getElementById(
+"search"
+).addEventListener(
+"input",
+function(){
+
+let keyword=
+this.value.toLowerCase();
+
+let filtered=
+allNews.filter(news=>
+
+news.title.toLowerCase()
+.includes(keyword)
+
+);
+
+renderNews(filtered);
+
+}
+);
+
+document.getElementById(
+"categoryFilter"
+).addEventListener(
+"change",
+function(){
+
+let category=
+this.value;
+
+if(category==="all"){
+renderNews(allNews);
+return;
+}
+
+let filtered=
+allNews.filter(news=>
+
+(news.category || "")
+===category
+
+);
+
+renderNews(filtered);
+
+}
+);
+
+document.getElementById(
+"darkModeBtn"
+).addEventListener(
+"click",
+()=>{
+
+document.body.classList.toggle(
+"dark"
+);
+
+}
+);
 document.getElementById(
 "districtFilter"
 ).addEventListener(
@@ -21,4 +145,28 @@ news.district===district
 renderNews(filtered);
 
 }
-);
+);let kannada=false;
+
+document.getElementById(
+"langBtn"
+).addEventListener(
+"click",
+()=>{
+
+kannada=!kannada;
+
+if(kannada){
+
+document.querySelector("h1")
+.innerText=
+"📰 ನಮ್ಮ ಕರ್ನಾಟಕ ಸುದ್ದಿ";
+
+}else{
+
+document.querySelector("h1")
+.innerText=
+"📰 Namma Karnataka News";
+
+}
+
+});
